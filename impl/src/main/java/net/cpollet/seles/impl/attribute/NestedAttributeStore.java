3f7fde5139d66afed69196d239eb62b7fc76b6b2
Christophe Pollet <cpollet@users.noreply.github.com>
2019-04-18 00:58:36 +0200

    removed useless import

--- impl/src/main/java/net/cpollet/seles/impl/attribute/NestedAttributeStore.java
+++ impl/src/main/java/net/cpollet/seles/impl/attribute/NestedAttributeStore.java
@@ -25,7 +25,6 @@ import net.cpollet.seles.impl.methods.NestedMethod;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
